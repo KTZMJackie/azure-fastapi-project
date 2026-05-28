6975e96895f3a026b0e4a5bd7bd490f43f4e52e6
Khine Thazin Myint (Jackie) <71181980+KTZMJackie@users.noreply.github.com>
2026-05-28 23:33:49 +0800

    Update provider.tf
    fix : add required_providers block with explicit version constraint

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.65"
+    }
+  }
+  required_version = ">= 1.5.0"
+}
+
 provider "azurerm" {
   features {}
 }
