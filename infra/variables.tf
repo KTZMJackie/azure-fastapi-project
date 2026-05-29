d7029b3eeba28e40f9389e6ff7b1854da1f63022
Khine Thazin Myint (Jackie) <71181980+KTZMJackie@users.noreply.github.com>
2026-05-29 13:31:09 +0800

    Update variables.tf
    fix : add api_key as sensitive terraform variable

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,8 @@ variable "acr_password" {
   description = "ACR admin password"
   sensitive   = true
 }
+
+variable "api_key" {
+  description = "API key for the FastAPI write endpoint"
+  sensitive   = true
+}
